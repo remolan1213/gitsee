9572401d501e6523f8f5018a3fb6fae22b818e46
remolan1213 <r.lanugon@gmail.com>
2024-12-02 09:19:29 +0800

    Changes in css deleted unused, app.css padding 0 at top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css"; // Import the CSS file
 
 function App() {
   const [commits, setCommits] = useState([]);
@@ -29,26 +30,53 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Git Commits</h1>
-      <ul>
-        {commits.map((commit, index) => (
-          <li key={index}>{commit.message}</li>
-        ))}
-      </ul>
-      <h2>Files</h2>
-      <input
-        type="text"
-        value={path}
-        onChange={handlePathChange}
-        placeholder="Enter repository path"
-      />
-      <button onClick={fetchFiles}>Fetch Files</button>
-      <ul>
-        {files.map((file, index) => (
-          <li key={index}>{file.path}</li>
-        ))}
-      </ul>
+    <div className="container">
+      <h1>DreamArchers Gitsee</h1>
+      <div className="section">
+        <h2 className="section-title text-left">Git Commits</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {commits.map((commit, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{commit.message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="section">
+        <h2 className="section-title text-left">Files</h2>
+        <input
+          type="text"
+          value={path}
+          onChange={handlePathChange}
+          placeholder="Enter repository path"
+        />
+        <button onClick={fetchFiles}>Fetch Files</button>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Path</th>
+            </tr>
+          </thead>
+          <tbody>
+            {files.map((file, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{file.path}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
